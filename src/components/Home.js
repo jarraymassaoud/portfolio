b98d43d790836070a0582e7634d5eff8aa317134
jarray massaoud <jarraymassoud@gmail.com>
2023-03-24 05:31:12 +0100

    background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Typed from "react-typed";
-import logo from "../assets/images/mylogo.jpg";
 
 export const Home = () => {
   return (
@@ -8,18 +7,19 @@ export const Home = () => {
       <div className="header-wraper">
         <div className="main-info">
           <p> Massaoud Jarray</p>
-          <img src={logo} alt="logo..." height="50%" width="10%" />
-
           <Typed
             className="typed-text"
-            strings={["Computer Engineer", "Developer FullStack Mern"]}
+            strings={[" Front-End Developer -Reactjs-"]}
             typeSpeed={40}
             backSpeed={60}
             loop
           />
-          <a href="/Massaoudj_cv.pdf" download>
-            Download CV
-          </a>
+          <div className="main-cv">
+            <a href="/Massaoudj_cv.pdf" download>
+              Download CV
+              <i class="fa fa-download" aria-hidden="true"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,39 +8,33 @@ export const Portfolio = () => {
   };
 
   return (
-    <div>
-      <div className="portfolio" id="portfolio">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <h2 className="dark-color">My Projects</h2>
-            </div>
+    <div className="portfolio" id="portfolio">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <h2 className="dark-color">My Projects</h2>
+          </div>
 
-            <div className="col-md-4">
-              <div
-                className={`logoport ${selected === 0 ? "selected" : ""}`}
-                onClick={() => handleSelect(0)}
-              >
-                <img src={logo2} alt="Logo 1" />
-              </div>
+          <div className="col-md-4">
+            <div
+              className={`logoport ${selected === 0 ? "selected" : ""}`}
+              onClick={() => handleSelect(0)}
+            >
+              <img src={logo2} alt="Logo 1" />
             </div>
-            <div className="col-md-4">
-              <div
-                className={`logoport ${selected === 1 ? "selected" : ""}`}
-                onClick={() => handleSelect(1)}
-              >
-                <img src={logo2} alt="Logo 2" />
-              </div>
-            </div>
+          </div>
+          <div className="col-md-4">
+            <div
+              className={`logoport ${selected === 1 ? "selected" : ""}`}
+              onClick={() => handleSelect(1)}
+            ></div>
+          </div>
 
-            <div className="col-md-4">
-              <div
-                className={`logoport ${selected === 2 ? "selected" : ""}`}
-                onClick={() => handleSelect(2)}
-              >
-                <img src={logo2} alt="Logo 3" />
-              </div>
-            </div>
+          <div className="col-md-4">
+            <div
+              className={`logoport ${selected === 2 ? "selected" : ""}`}
+              onClick={() => handleSelect(2)}
+            ></div>
           </div>
         </div>
       </div>
